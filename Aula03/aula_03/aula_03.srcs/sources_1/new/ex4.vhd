f87434a5fd65978b9ff4bbf7fd66287d9d4ef248
Rodrigo Cunha <rodrigocunha@ua.pt>
2016-02-27 15:54:00 +0000

    vhdls criados

---
+++ Aula03/aula_03/aula_03.srcs/sources_1/new/ex4.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/27/2016 03:53:20 PM
+-- Design Name: 
+-- Module Name: ex4 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ex4 is
+--  Port ( );
+end ex4;
+
+architecture Behavioral of ex4 is
+
+begin
+
+
+end Behavioral;
---
+++ Aula03/aula_03/aula_03.srcs/sources_1/new/ex5.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/27/2016 03:53:33 PM
+-- Design Name: 
+-- Module Name: ex5 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ex5 is
+--  Port ( );
+end ex5;
+
+architecture Behavioral of ex5 is
+
+begin
+
+
+end Behavioral;
