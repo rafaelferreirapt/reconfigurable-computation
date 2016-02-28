025f86d042a64c36d80d28d6ffd45d89ade40b29
Bruno Silva <brunomiguelsilva@ua.pt>
2016-02-28 23:16:36 +0000

    ex4 hopefully done

--- Aula03/aula_03/aula_03.srcs/sources_1/new/ex4.vhd
+++ Aula03/aula_03/aula_03.srcs/sources_1/new/ex4.vhd
@@ -21,6 +21,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,12 +34,90 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity ex4 is
---  Port ( );
+Port ( sw : in std_logic_vector (15 downto 0);
+       seg : out STD_LOGIC_VECTOR (6 downto 0);
+       an : out STD_LOGIC_VECTOR (7 downto 0);
+       led : out STD_LOGIC_VECTOR (8 downto 0));
 end ex4;
 
 architecture Behavioral of ex4 is
+signal divided_clk  : std_logic;
+variable res : std_logic_vector(8 downto 0);
 
+procedure display(variable input : std_logic_vector (8 downto 0);
+                  variable sw1   : std_logic_vector (7 downto 0);
+                  variable sw2   : std_logic_vector (7 downto 0);
+                  variable op    : std_logic_vector (1 downto 0))
+is 
+begin
+if sw2 = "00000000" and op = "11" then
+    seg <= "1000000";
+    an <= "11111110";
+elsif (to_integer(unsigned(sw1)) < to_integer(unsigned(sw2))) and op = "01" then
+    seg <= "0111111";
+    an <= "01111111";
+    led <= input;
+else
+    led <= input;
+end if;
+end display;
+
+function add(input: std_logic_vector (15 downto 0)) return std_logic_vector 
+    is variable res: std_logic_vector (8 downto 0);
+    
+begin
+    
+    res <= std_logic_vector(unsigned(input(15 downto 8) + unsigned(input(7 downto 0)));
+    
+    return res;
+end add;
+
+function sub(input: std_logic_vector (15 downto 0)) return std_logic_vector 
+    is variable res: std_logic_vector (8 downto 0);
+    
+begin
+    
+    res <= std_logic_vector(unsigned(input(15 downto 8) - unsigned(input(7 downto 0)));
+    
+    return res;
+end sub;
+
+function mult(input: std_logic_vector (15 downto 0)) return std_logic_vector 
+    is variable res: std_logic_vector (8 downto 0);
+    
+begin
+    
+    res <= std_logic_vector(unsigned(input(15 downto 8) * unsigned(input(7 downto 0)));
+    
+    return res;
+end mult;
+
+function div(input: std_logic_vector (15 downto 0)) return std_logic_vector 
+    is variable res: std_logic_vector (8 downto 0);
+    
+begin
+    
+    res <= std_logic_vector(unsigned(input(15 downto 8) / unsigned(input(7 downto 0)));
+    
+    return res;
+end div;
+    
 begin
 
+process(divided_clk)
+begin
+res := add(sw);
+display(res,sw(15 downto 8),sw(7 downto 0),"00");
+res := sub(sw);
+display(res,sw(15 downto 8),sw(7 downto 0),"01");
+res := mult(sw);
+display(res,sw(15 downto 8),sw(7 downto 0),"10");
+res := div(sw);
+display(res,sw(15 downto 8),sw(7 downto 0),"11");
+
+end process;
+
+div:    entity xil_defaultlib.clock_divider
+        port map (clk, divided_clk);
 
 end Behavioral;
