bcaa51c670546b93cde08f474a48e2984438c5ff
Bernardomrf <bernardomrferreira@ua.pt>
2016-03-01 11:29:08 +0000

    ex5

--- Aula03/aula_03/aula_03.runs/synth_1/rundef.js
+++ Aula03/aula_03/aula_03.runs/synth_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 ISEStep( "vivado",
-         "-log ex3.vds -m64 -mode batch -messageDb vivado.pb -notrace -source ex3.tcl" );
+         "-log full_adder.vds -m64 -mode batch -messageDb vivado.pb -notrace -source full_adder.tcl" );
 
 
 
