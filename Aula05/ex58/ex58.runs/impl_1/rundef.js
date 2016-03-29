51373b799d5379865f9ab8f608be7eae7a26d4fa
Rafael Ferreira <mail@rafaelferreira.pt>
2016-03-29 11:31:44 +0100

    ex8

--- Aula05/ex58/ex58.runs/impl_1/rundef.js
+++ Aula05/ex58/ex58.runs/impl_1/rundef.js
@@ -1,16 +1,16 @@
 //
 // Vivado(TM)
 // rundef.js: a Vivado-generated Runs Script for WSH 5.1/5.6
-// Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+// Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
 //
 
 var WshShell = new ActiveXObject( "WScript.Shell" );
 var ProcEnv = WshShell.Environment( "Process" );
 var PathVal = ProcEnv("PATH");
 if ( PathVal.length == 0 ) {
-  PathVal = "C:/Xilinx/Vivado/2015.4/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2015.4/ids_lite/ISE/lib/nt64;C:/Xilinx/Vivado/2015.4/bin;";
+  PathVal = "C:/Xilinx/Vivado/2014.4/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2014.4/ids_lite/ISE/lib/nt64;C:/Xilinx/Vivado/2014.4/bin;";
 } else {
-  PathVal = "C:/Xilinx/Vivado/2015.4/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2015.4/ids_lite/ISE/lib/nt64;C:/Xilinx/Vivado/2015.4/bin;" + PathVal;
+  PathVal = "C:/Xilinx/Vivado/2014.4/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2014.4/ids_lite/ISE/lib/nt64;C:/Xilinx/Vivado/2014.4/bin;" + PathVal;
 }
 
 ProcEnv("PATH") = PathVal;
@@ -25,7 +25,7 @@ eval( EAInclude(ISEJScriptLib) );
 // pre-commands:
 ISETouchFile( "init_design", "begin" );
 ISEStep( "vivado",
-         "-log ex1.vdi -applog -m64 -messageDb vivado.pb -mode batch -source ex1.tcl -notrace" );
+         "-log use_Iterative_Sorter_FSM.vdi -applog -m64 -messageDb vivado.pb -mode batch -source use_Iterative_Sorter_FSM.tcl -notrace" );
 
 
 
