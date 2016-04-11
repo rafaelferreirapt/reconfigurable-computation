484849b139c710040ba84e6ecd236078879e6f99
Rafael Ferreira <mail@rafaelferreira.pt>
2016-04-11 22:52:25 +0100

    ex4 e adicionei mais IP ao repositorio

--- Aula08/Projects2016/UserDefinedRepository/TopCounter32/TopCounter32.srcs/sources_1/new/TopCounter32.vhd
+++ Aula08/Projects2016/UserDefinedRepository/TopCounter32/TopCounter32.srcs/sources_1/new/TopCounter32.vhd
@@ -3,9 +3,10 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.STD_LOGIC_unsigned.all;
 
 entity TopCounter32 is
-    generic (size : integer := 32);
+    generic (size : integer := 32;
+             size_in : integer := 32);
     port ( clk : in std_logic;
-           btnC : in std_logic;
+           data_in : in std_logic_vector (size_in-1 downto 0);
            data_out : out std_logic_vector (size-1 downto 0));
 end TopCounter32;
 
